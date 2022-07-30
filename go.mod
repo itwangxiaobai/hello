011157fdcf8f07442a384dab022de9a038091f4a
wangxiaobai <it_wangxiaobai@163.com>
2022-07-30 16:10:49 +0800

    feat:SayHi现在支持给指定人打招呼啦

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wangxiaobai/hello
+module github.com/wangxiaobai/hello/v2
 
 go 1.17
--- hello.go
+++ hello.go
@@ -2,6 +2,8 @@ package hello
 
 import "fmt"
 
-func SayHi() {
-	fmt.Println("你好，我是王小白。很高兴认识你！")
+// SayHi向指定人打招呼的函数
+
+func SayHi(name string) {
+	fmt.Printf("你好%s，我是王小白。很高兴认识你！\n", name)
 }
